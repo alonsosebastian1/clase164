be492781a196f659cbdf53fcffba2f99521bdb8b
alonsosebastian1 <93092606+alonsosebastian1@users.noreply.github.com>
2023-07-21 12:06:30 -0600

    Add files via upload

---
+++ playermove.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent("player-move",{
+    init:function(){
+        this.walk();
+    
+    },
+    walk:function(){
+        window.addEventListener("keydown",(e)=>{
+            if(
+                e.key == "ArrowUp" || 
+                e.key == "ArrowDown"  || 
+                e.key == "ArrowRight" || 
+                e.key == "ArrowLeft"
+            ){
+                var entity = document.querySelector("#sound2");
+                entity.components.sound.playSound()
+            }
+        })
+    }
+})
